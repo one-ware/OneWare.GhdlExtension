3c5c70f57ceb7a24de99fc833001a7ca378117ab
Hendrik Mennen <hmennen@protop-solutions.com>
2023-08-06 17:37:52 +0200

    init

---
+++ OneWare.Ghdl/Views/GhdlMainWindowToolBarExtension.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace OneWare.Ghdl.Views;
+
+public partial class GhdlMainWindowToolBarExtension : UserControl
+{
+    public GhdlMainWindowToolBarExtension()
+    {
+        InitializeComponent();
+    }
+}
