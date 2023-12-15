a949e9e616e94e4ff78ccbed97eca2f3f4b63202
Hendrik Mennen <hmennen@protop-solutions.com>
2023-12-15 11:47:41 +0100

    fix

--- src/OneWare.Ghdl/GhdlModule.cs
+++ src/OneWare.Ghdl/GhdlModule.cs
@@ -28,15 +28,15 @@ public class GhdlModule : IModule
 
         nativeTool.AddPlatform(PlatformId.WinX64,
                 "https://github.com/ghdl/ghdl/releases/download/v3.0.0/ghdl-MINGW32.zip")
-            .WithShortcut("ghdlExecutable", "GHDL/bin/ghdl.exe");
+            .WithShortcut("ghdlExecutable", "GHDL/bin/ghdl.exe", GhdlPathSetting);
         
         nativeTool.AddPlatform(PlatformId.LinuxX64,
                 "https://cdn.vhdplus.com/ghdl/ghdl2.0.0-ubuntu20.zip")
-            .WithShortcut("ghdlExecutable", "linux-x64/ghdl/bin/ghdl");
+            .WithShortcut("ghdlExecutable", "ghdl/bin/ghdl", GhdlPathSetting);
         
         nativeTool.AddPlatform(PlatformId.OsxX64,
                 "https://github.com/ghdl/ghdl/releases/download/v3.0.0/ghdl-macos-11-mcode.tgz")
-            .WithShortcut("ghdlExecutable", "osx-x64/ghdl/bin/ghdl");
+            .WithShortcut("ghdlExecutable", "ghdl/bin/ghdl", GhdlPathSetting);
         
         containerProvider.Resolve<ISettingsService>().RegisterTitledPath("Simulator", "GHDL", GhdlPathSetting, "GHDL Path", "Path for GHDL executable", 
             nativeTool.GetShortcutPathOrEmpty("ghdlExecutable"),
