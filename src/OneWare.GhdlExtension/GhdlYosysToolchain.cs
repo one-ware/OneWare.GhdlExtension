fcbd6276a355b4a84aa8cb0dbcac215b691b1892
Marcel Ploemacher <marcel.ploemacher@smail.th-koeln.de>
2025-06-30 14:56:20 +0200

    Generated Toolchain Stub

---
+++ src/OneWare.GhdlExtension/GhdlYosysToolchain.cs
@@ -0,0 +1,32 @@
+﻿using OneWare.GhdlExtension.Services;
+using OneWare.UniversalFpgaProjectSystem.Models;
+using OneWare.UniversalFpgaProjectSystem.Services;
+
+namespace OneWare.GhdlExtension;
+
+public class GhdlYosysToolchain(GhdlService ghdlService) : IFpgaToolchain
+{
+    public void OnProjectCreated(UniversalFpgaProjectRoot project)
+    {
+    }
+
+    public void LoadConnections(UniversalFpgaProjectRoot project, FpgaModel fpga)
+    {
+        //Code aus Yosys-Toolchain kopieren oder sie direkt rufen?
+        throw new NotImplementedException();
+    }
+
+    public void SaveConnections(UniversalFpgaProjectRoot project, FpgaModel fpga)
+    {
+        //Code aus Yosys-Toolchain kopieren oder sie direkt rufen?
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CompileAsync(UniversalFpgaProjectRoot project, FpgaModel fpga)
+    {
+        //Code aus Yosys-Toolchain kopieren oder sie direkt rufen?
+        throw new NotImplementedException();
+    }
+
+    public string Name => "GHDL_Yosys";
+}
