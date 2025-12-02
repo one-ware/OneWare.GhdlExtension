6cf7bbf9dba1a43446c1e9884551df016cb110ea
Marcel Ploemacher <marcel.ploemacher@smail.th-koeln.de>
2025-12-02 15:13:12 +0100

    Initial Refactor

--- src/OneWare.GhdlExtension/GhdlExtensionModule.cs
+++ src/OneWare.GhdlExtension/GhdlExtensionModule.cs
@@ -200,11 +200,12 @@ public class GhdlExtensionModule : IModule
     public void RegisterTypes(IContainerRegistry containerRegistry)
     {
         containerRegistry.RegisterSingleton<GhdlService>();
+        containerRegistry.RegisterSingleton<GhdlToolchainService>();
     }
 
     public void OnInitialized(IContainerProvider containerProvider)
     {
-        var yosysService = containerProvider.Resolve<YosysService>();
+        var ghdlToolchainService = containerProvider.Resolve<GhdlToolchainService>();
         var windowService = containerProvider.Resolve<IWindowService>();
         var projectExplorerService = containerProvider.Resolve<IProjectExplorerService>();
         var fpgaService = containerProvider.Resolve<FpgaService>();
@@ -372,7 +373,6 @@ public class GhdlExtensionModule : IModule
         _projectExplorerService = containerProvider.Resolve<IProjectExplorerService>();
         
         containerProvider.Resolve<FpgaService>().RegisterToolchain<GhdlYosysToolchain>();
-        GhdlYosysToolchain.SubscribeToSettings(containerProvider.Resolve<ISettingsService>());
         
         
         
@@ -411,20 +411,7 @@ public class GhdlExtensionModule : IModule
                                 Command = new AsyncRelayCommand(async () =>
                                 {
                                     await projectExplorerService.SaveOpenFilesForProjectAsync(root);
-                                    var fpgaModel = new FpgaModel(fpga!); 
-                                    await ghdlPreCompiler.PerformPreCompileStepAsync(root, fpgaModel);
-                                    
-                                    try{
-                                        var verilogFileName = ghdlPreCompiler.VerilogFileName ?? throw new Exception("Invalid verilog file name!");
-                                        var ghdlOutputPath = Path.Combine(root.FullPath, ghdlPreCompiler.BuildDir,
-                                            ghdlPreCompiler.GhdlOutputDir, verilogFileName);
-                                        var mandatoryFileList = new List<string>(1) {ghdlOutputPath};
-                                        await yosysService.SynthAsync(root, new FpgaModel(fpga!), mandatoryFileList);
-                                    }
-                                    catch (Exception e)
-                                    { 
-                                        ContainerLocator.Container.Resolve<ILogger>().Error(e.Message, e);
-                                    }
+                                    await ghdlToolchainService.SynthAsync(root, new FpgaModel(fpga!));
                                     
                                 }, () => fpga != null)
                             },
@@ -434,7 +421,7 @@ public class GhdlExtensionModule : IModule
                                 Command = new AsyncRelayCommand(async () =>
                                 {
                                     await projectExplorerService.SaveOpenFilesForProjectAsync(root);
-                                    await yosysService.FitAsync(root, new FpgaModel(fpga!));
+                                    await ghdlToolchainService.FitAsync(root, new FpgaModel(fpga!));
                                 }, () => fpga != null)
                             },
                             new MenuItem()
@@ -443,7 +430,7 @@ public class GhdlExtensionModule : IModule
                                 Command = new AsyncRelayCommand(async () =>
                                 {
                                     await projectExplorerService.SaveOpenFilesForProjectAsync(root);
-                                    await yosysService.AssembleAsync(root, new FpgaModel(fpga!));
+                                    await ghdlToolchainService.AssembleAsync(root, new FpgaModel(fpga!));
                                 }, () => fpga != null)
                             },
                             new Separator(),
---
+++ src/OneWare.GhdlExtension/Services/GhdlToolchainService.cs
@@ -0,0 +1,70 @@
+﻿using OneWare.Essentials.Services;
+using OneWare.OssCadSuiteIntegration.Yosys;
+using OneWare.UniversalFpgaProjectSystem.Models;
+using Prism.Ioc;
+
+namespace OneWare.GhdlExtension.Services;
+
+public class GhdlToolchainService
+{
+    private static string? _val;
+    private readonly GhdlVhdlToVerilogPreCompileStep _ghdlPreCompiler;
+    private readonly YosysService _yosysService;
+
+    public GhdlToolchainService(GhdlVhdlToVerilogPreCompileStep ghdlPreCompiler, YosysService yosysService, ISettingsService settingsService)
+    {
+        _ghdlPreCompiler = ghdlPreCompiler;
+        _yosysService = yosysService;
+        
+        settingsService.GetSettingObservable<string>("OssCadSuite_Path").Subscribe(x => _val = x);
+
+    }
+    
+    
+    public async Task<bool> CompileAsync(UniversalFpgaProjectRoot project, FpgaModel fpga)
+    {
+        var success = await SynthAsync(project, fpga);
+        success = success && await FitAsync(project, fpga);
+        success = success && await AssembleAsync(project, fpga);
+        return success;
+    }
+
+    public async Task<bool> SynthAsync(UniversalFpgaProjectRoot project, FpgaModel fpga)
+    {
+        if (_val == null)
+        {
+            ContainerLocator.Container.Resolve<ILogger>().Error("Yosys binary not found");
+            return false;
+        }
+
+        bool success = await _ghdlPreCompiler.PerformPreCompileStepAsync(project, fpga);
+        if (!success) return false;
+        
+       
+        try
+        {
+            var verilogFileName = _ghdlPreCompiler.VerilogFileName ?? throw new Exception("Invalid verilog file name!");
+            var ghdlOutputPath = Path.Combine(project.FullPath, _ghdlPreCompiler.BuildDir, 
+                _ghdlPreCompiler.GhdlOutputDir, verilogFileName);
+            var mandatoryFileList = new List<string>(1) {ghdlOutputPath};
+          
+            success = await _yosysService.SynthAsync(project, fpga, mandatoryFileList);
+            return success;
+        }
+        catch (Exception e)
+        {
+            ContainerLocator.Container.Resolve<ILogger>().Error(e.Message, e);
+            return false;
+        }
+    }
+
+    public async Task<bool> FitAsync(UniversalFpgaProjectRoot project, FpgaModel fpga)
+    {
+        return await _yosysService.FitAsync(project, fpga);
+    }
+
+    public async Task<bool> AssembleAsync(UniversalFpgaProjectRoot project, FpgaModel fpga)
+    {
+        return await _yosysService.AssembleAsync(project, fpga);
+    }
+}
